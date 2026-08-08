8687872e7196991ffdbabbbeb40e35eea774ffeb
Developer <dev@placeholder.local>
2026-08-08 18:59:33 +0330

    new ui design

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -26,7 +26,7 @@ async function submit() {
     router.push(redirect)
   } catch (e) {
     const msg = e.response?.data?.message || e.message || 'خطای ناشناخته'
-    error.value = 'نام کاربری یا رمز عبور اشتباه است. (' + msg + ')'
+    error.value = 'ddddddddddddd (' + msg + ')'
   } finally {
     loading.value = false
   }
